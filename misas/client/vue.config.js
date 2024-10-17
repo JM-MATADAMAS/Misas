22c150bb550b681f8aa589168e3ba44243677097
Javier Matadamas <javier.matadamas2001@gmail.com>
2024-10-17 09:36:21 -0600

    Pages test 3

--- misas/client/vue.config.js
+++ misas/client/vue.config.js
@@ -7,6 +7,6 @@ module.exports = defineConfig({
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/Misas/misas/client/'
+    ? '/Misas/misas/client/src/pages'
     : '/'
 }
